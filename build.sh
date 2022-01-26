7c99fd96917cbdd6c238a5fb9e69365da67e326f
user <user@pm.me>
2022-01-26 09:17:57 -0700

    assignment_! dir made

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-
-docker build --platform linux/amd64 -t glesica/commander .
-
--- commander.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-set -e
-
-docker run -u $UID -v $PWD:/data glesica/commander commander "$@"
-
