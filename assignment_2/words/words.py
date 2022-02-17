fd0fc45c8b96a49ad6393ae751c69342c7d2fa98
user <user@pm.me>
2022-02-17 16:09:15 -0700

    Began working on doctests for split_words()

--- assignment_2/words/words.py
+++ assignment_2/words/words.py
@@ -5,6 +5,18 @@ def split_words(sentence: str) -> List[str]:
     """
     Split a string into a list of words. A word boundary is defined to be any
     number of space or tab characters.
+    >>> split_words("Hello World")
+    ['Hello', 'World']
+    >>> split_words("Tab\tTest")
+    ['Tab', 'Test']
+    >>> split_words("Multiple                   Spaces")
+    ['Multiple', 'Spaces']
+    >>> split_words("Multiple\t\t\t\t\t\tTabs")
+    ['Multiple', 'Tabs']
+    >>> split_words("Now we are testing\ta combination \t of tabs\tand \tspaces.")
+    ['Now', 'we', 'are', 'testing', 'a', 'combination', 'of', 'tabs', 'and', 'spaces.']
+    >>> split_words("")
+    []
     """
     words = []
     next_word = ""
