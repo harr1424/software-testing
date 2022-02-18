522b0dda2e8bfd581aef783a0dc9b4191a1d1595
user <user@user>
2022-02-17 18:11:24 -0700

    Doctests complete, TODO item created for unit testing negative tests

--- assignment_2/words/words.py
+++ assignment_2/words/words.py
@@ -1,5 +1,17 @@
 from typing import List
 
+"""
+John Harrington
+Software Reliability and Testing, Spring, 2022
+Assignment Two 
+
+In this assignment submission, doctests are preferentially used to highlight intended use cases of functions 
+and so the tests written as doctests are intended to succeed. Tests have also been written which are intended 
+to fail, and these are included as unit tests at the end of the file. Unit tests have been used preferentially 
+for tests that should fail because these tests involve asserting unhandled exceptions and errors and so including 
+these tests as unit tests instead of doctests makes this submission significantly more readable
+"""
+
 
 def split_words(sentence: str) -> List[str]:
     """
@@ -16,6 +28,8 @@ def split_words(sentence: str) -> List[str]:
     >>> split_words("Now we are testing\ta combination \t of tabs\tand \tspaces.")
     ['Now', 'we', 'are', 'testing', 'a', 'combination', 'of', 'tabs', 'and', 'spaces.']
     >>> split_words("")
+    []
+    >>> split_words("\t")
     []
     """
     words = []
@@ -39,6 +53,14 @@ def join_words(words: List[str]) -> str:
     """
     Take a list of words and return it as a sentence, with adjacent words
     separated by a single space character.
+    >>> join_words(['Hello', 'World'])
+    'Hello World'
+    >>> join_words(['Hello', 'world', 'how', 'are', 'you', 'today?'])
+    'Hello world how are you today?'
+    >>> join_words([])
+    ''
+    >>> join_words(['Empty', '', 'strings', '', 'yield', '', 'spaces.'])
+    'Empty  strings  yield  spaces.'
     """
     sentence = ""
 
@@ -54,5 +76,21 @@ def count_words(sentence: str) -> int:
     """
     Count the number of words in the sentence. A word boundary is defined to be
     any number of space or tab characters.
+    >>> count_words("Hello world!")
+    2
+    >>> count_words('')
+    0
+    >>> count_words("\t")
+    0
+    >>> count_words("Multipls spaces          should be treated   as one space.")
+    8
+    >>> count_words("Testing \ttabs\t and\t \t spaces")
+    4
     """
     return len(split_words(sentence))
+
+
+
+# TODO use unit tsting to write tests that should fails
+# Explore asserting unhandled exceptions
+# Also test extremely large strings using project Gutenberg
