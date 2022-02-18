c12b2d365333eb32b903a3b936d5f96ccdfcd9b0
user <user@pm.me>
2022-02-18 09:50:32 -0700

    Added unit tests

--- assignment_2/words/words.py
+++ assignment_2/words/words.py
@@ -11,6 +11,10 @@ In this assignment submission, doctests are preferentially used to highlight int
 and so the tests written as doctests are intended to succeed. Tests have also been written which are intended 
 to fail, and these are included as unit tests at the end of the file. Unit tests have been used preferentially 
 for tests that should fail because these tests involve asserting unhandled exceptions.
+
+KNOWN ISSUES: 
+join_words() is defined to accept a list of strings. If this function is passed a single string, 
+instead of raising a TypeError, it will return the string with a single space added between each character.
 """
 
 
@@ -91,16 +95,65 @@ def count_words(sentence: str) -> int:
     return len(split_words(sentence))
 
 
-# TODO use unit testing to write tests that should fail
+# Use unit testing to write tests that should fail
+
+test_dict = {
+    "a" : 1,
+    "b" : 2,
+    "c" : 3,
+    "d" : 4
+}
 
 def test_split_words_illegal_arg_type():
     with pytest.raises(TypeError):
-        split_words(108) == 108
+        split_words(108)
 
 
 def test_split_words_illegal_num_args():
     with pytest.raises(TypeError):
         split_words("Hello", "World")
+
+
+def test_join_words_illegal_arg_type_string():
+    with pytest.raises(TypeError):
+        join_words("This, should, be, a, list")
+        """
+        This test does not perform as expected, and indicates an error with the join_words
+        function. If a string is passed to this function, instead of raising a TypeError, 
+        join_words will return the string with an added space in between each character of the string.
+        """
+
+def test_join_words_illegal_arg_type_int():
+    with pytest.raises(TypeError):
+        join_words(108)
+
+
+def test_join_words_illegal_arg_type_boolean():
+    with pytest.raises(TypeError):
+        join_words(False)
+
+
+def test_join_words_illegal_arg_type_dict():
+    with pytest.raises(TypeError):
+        join_words(test_dict)
+        """
+        This test does not perform as expected, and indicates an error with the join_words
+        function. If a dictionary is passed to this function, instead of raising a TypeError, 
+        join_words will return the dictionary keys.
+        """
+
+def test_join_Words_illegal_arg_type_tuple():
+    with pytest.raises(TypeError):
+        join_words(("This", "should", "be", "a", "list."))
+        """
+        This test does not perform as expected, and indicates an error with the join_words
+        function. If a tuple is passed to this function, instead of raising a TypeError, 
+        join_words will return the tuple with an added space in between each item of the tuple.
+        """
+
+def test_join_words_illegal_num_args():
+    with pytest.raises(TypeError):
+        join_words(['list', 'one'], ['list', 'two'])
 
 
 
