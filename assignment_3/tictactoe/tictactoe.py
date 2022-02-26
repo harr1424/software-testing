7a1cceee155fc42a9e324336de8299d37d8c3c6d
user <user@user>
2022-02-26 15:22:15 -0700

    Finished extracting dependencies, TODO implement tests

--- assignment_3/tictactoe/tictactoe.py
+++ assignment_3/tictactoe/tictactoe.py
@@ -1,43 +1,80 @@
 import re
 
 
-# Helper method to take turns
-def take_turn(curr_player):
-    if curr_player == 'X':
-        return 'O'
-    elif curr_player == 'O':
-        return 'X'
-    else:
-        raise RuntimeError("Current player was not defined as either 'X' or 'O'")
+class Helper:
+    """
+    This class contains helper methods that can be injected into the Game class
+    """
 
+    def __init__(self):
+        # Store configs for winner_check() method
+        self.configs = [
+            [(0, 0), (0, 1), (0, 2)],
+            [(1, 0), (1, 1), (1, 2)],
+            [(2, 0), (2, 1), (2, 2)],
+            [(0, 0), (1, 0), (2, 0)],
+            [(0, 1), (1, 1), (2, 1)],
+            [(0, 2), (1, 2), (2, 2)],
+            [(0, 0), (1, 1), (2, 2)],
+            [(2, 0), (1, 1), (0, 2)],
+        ]
 
-# Helper method to print board
-def print_board(board):
-    print("  1 2 3")
-    for i in range(3):
-        print(f"{i + 1} " + "|".join(board[i]))
+    # Helper method to take turns
+    def take_turn(self, curr_player):
+        if curr_player == 'X':
+            return 'O'
+        elif curr_player == 'O':
+            return 'X'
+        else:
+            raise RuntimeError("Current player was not defined as either 'X' or 'O'")
 
+    # Helper method to print board
+    def print_board(self, board):
+        print("  1 2 3")
+        for i in range(3):
+            print(f"{i + 1} " + "|".join(board[i]))
 
-# Accept the current player's next move, validate it,
-# and update the board's state
-def next_move(curr_player, board):
-    move = input(f"{curr_player}'s move, enter row and column: ")
-    row, col = re.split(r"[ \t,]+", move)
-    row_index, col_index = int(row) - 1, int(col) - 1
-
-    while board[row_index][col_index] != " ":
-        print("Choose an empty space!")
-        print_board(board)
+    # Accept the current player's next move, validate it,
+    # and update the board's state
+    def next_move(self, curr_player, board):
+        # Get next move from user
         move = input(f"{curr_player}'s move, enter row and column: ")
         row, col = re.split(r"[ \t,]+", move)
         row_index, col_index = int(row) - 1, int(col) - 1
-        continue
 
-    board[row_index][col_index] = curr_player
+        # Validate the move
+        while board[row_index][col_index] != " ":
+            print("Choose an empty space!")
+            self.print_board(board)
+            move = input(f"{curr_player}'s move, enter row and column: ")
+            row, col = re.split(r"[ \t,]+", move)
+            row_index, col_index = int(row) - 1, int(col) - 1
+            continue
+
+        # Update the board object
+        board[row_index][col_index] = curr_player
+
+    def winner_check(self, row_index, col_index, curr_player, board):
+        # Check for a winner
+        winner = False
+
+        for config in self.configs:
+            config_wins = True
+            for row_index, col_index in config:
+                if board[row_index][col_index] != curr_player:
+                    config_wins = False
+                    break
+            if config_wins:
+                winner = True
+                break
+        if winner:
+            print(f"Congratulations {curr_player}, you won!")
+            exit(0)
 
 
 class Game:
-    def __init__(self):
+    def __init__(self, helper_methods):
+        self.helper = helper_methods
         self.board = [
             [" ", " ", " "],
             [" ", " ", " "],
@@ -47,47 +84,24 @@ class Game:
         self.row_index = -1
         self.col_index = -1
 
-
-
     def play(self):
         while True:
             # Print the current board state
-            print_board(self.board)
+            self.helper.print_board(self.board)
 
             # Process next move
-            next_move(self.turn, self.board)
+            self.helper.next_move(self.turn, self.board)
 
             # Check for a winner
-            winner = False
-            configs = [
-                [(0, 0), (0, 1), (0, 2)],
-                [(1, 0), (1, 1), (1, 2)],
-                [(2, 0), (2, 1), (2, 2)],
-                [(0, 0), (1, 0), (2, 0)],
-                [(0, 1), (1, 1), (2, 1)],
-                [(0, 2), (1, 2), (2, 2)],
-                [(0, 0), (1, 1), (2, 2)],
-                [(2, 0), (1, 1), (0, 2)],
-            ]
-            for config in configs:
-                config_wins = True
-                for row_index, col_index in config:
-                    if self.board[row_index][col_index] != self.turn:
-                        config_wins = False
-                        break
-                if config_wins:
-                    winner = True
-                    break
-            if winner:
-                print(f"Congratulations {self.turn}, you won!")
-                break
+            self.helper.winner_check(self.row_index, self.col_index, self.turn, self.board)
 
             # There was no winner, so go to the next player
-            self.turn = take_turn(self.turn)
+            self.turn = self.helper.take_turn(self.turn)
 
 
 def main():
-    game = Game()
+    helper = Helper()
+    game = Game(helper)
     game.play()
 
 
